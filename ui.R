3281c1879f7fee61a1008c81a479d5d0f0524f44
Louise <louisegreen18@gmail.com>
2022-01-01 14:14:11 +0000

    buttons added to overview page
    buttons added to overview page

--- ui.R
+++ ui.R
@@ -5,35 +5,40 @@ ui <- fluidPage(
     tabPanel("Overview",
              
              # creates the element for the top row
-             fluidRow(column(width = 12),
-                      
-
+             fluidRow(column(width = 4,
+                      radioButtons("icu_choice_1", "ICU choice",
+                                   choices = c("Yes", "No"))),
+                      column(width = 4,
+                             numericInput("age_range_1", "Age Range",
+                                          value = 1, min = 1, max = 100),
+                             ),
+                      column(width = 4,
+                             dateRangeInput("date_range_1", "Date Range",
+                                            start = "2016-01-01",
+                                            end = "2022-01-01",
+                                          )
+                             )
+                    )
              ),
              
              # creates the element for the main row, first half
              fluidRow(column(width = 6),
                       
-              # creates the elemetn for the main row, second half
+              # creates the element for the main row, second half
                       column(width = 6)
                       
                       ),
-               plotOutput("whisky_plot"),
-               plotOutput("whisky_plot")
 
-             ),
-    
     # creates the navigation for ICU tab
-    tabPanel("ICU",
-             
+    tabPanel("ICU"
              ),
-    # creates the navigation for A&E tab
-    tabPanel("A&E",
              
+    # creates the navigation for A&E tab
+    tabPanel("A&E"
              ),
     
     # creates the navigation for statistics tab
-    tabPanel("Statistics",
-             
-             )
-  )
+    tabPanel("Statistics"
+             ),
+  ),          
 )
