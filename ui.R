89281838a66fe248a658a5058131c4f1c063838a
Louise <louisegreen18@gmail.com>
2022-01-01 13:28:33 +0000

    initial tabs added
    initial tabs added

--- ui.R
+++ ui.R
@@ -1,9 +1,27 @@
 ui <- fluidPage(
-  
-  fluidRow(
-  selectInput("region", "Region:",
-              choices = unique(whisky_df$Region))),
-  
-  leafletOutput("whisky_map")
-  
+  tabsetPanel(
+    
+    # creates the navigation for ICU tab
+    tabPanel("Overview",
+             
+             splitLayout(
+               plotOutput(),
+               plotOutput()
+             )
+             ),
+    
+    # creates the navigation for ICU tab
+    tabPanel("ICU",
+             
+             ),
+    # creates the navigation for A&E tab
+    tabPanel("A&E",
+             
+             ),
+    
+    # creates the navigation for statistics tab
+    tabPanel("Statistics",
+             
+             )
+  )
 )
