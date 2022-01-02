8682abfc1f0a4f7ac6bc62c1e3c736905d24a147
Louise <louisegreen18@gmail.com>
2022-01-02 15:11:58 +0000

    ICU & A&E completed
    ICU & A&E completed - plots added, printed text made clearer, comments in code tidied up

--- server.R
+++ server.R
@@ -1,13 +1,12 @@
 server <- function(input, output) {
   
-  # Placeholder example plot
+# placeholder input for the overview tab ---------------------------------------
   output$icu_plot <- renderPlot({
     activity_specialty %>%
       filter(specialty_name == "Intensive Care Medicine") %>%
       ggplot(aes(quarter)) +
       geom_histogram(stat = "count")
   })
-  
     
     output$cardio_plot <- renderPlot({
       activity_specialty %>%
@@ -15,13 +14,29 @@ server <- function(input, output) {
         ggplot(aes(quarter)) +
         geom_histogram(stat = "count") 
   })
-  
+    
+# placeholder input for the ICU tab --------------------------------------------
     output$dermatology_plot <- renderPlot({
       activity_specialty %>%
         filter(specialty_name == "Dermatology") %>%
         ggplot(aes(quarter)) +
         geom_histogram(stat = "count") 
     })
+
+    output$icu_text_placeholder <- renderText({
+      print("this is a placeholder text for the description of the plot in the ICU tab")
+    })
     
-    
+# placeholder input for the A&E tab --------------------------------------------
+    output$neurology_plot <- renderPlot({
+      activity_specialty %>%
+        filter(specialty_name == "Neurology") %>%
+        ggplot(aes(quarter)) +
+        geom_histogram(stat = "count") 
+    })
+
+    output$ae_text_placeholder <- renderText({
+      print("this is a placeholder text for the description of the plot in the A&E tab")
+    })
+
 }
--- ui.R
+++ ui.R
@@ -21,14 +21,12 @@ ui <- fluidPage(
                              )
                     ),
              
-             
-             # creates the element for the main row, first half (this is currently a placeholder as R
-             # does not show both plots when inputting two, but both will be plots)
+             # creates the element for the main row, first half (this is a placeholder plot)
              fluidRow(column(width = 6,
                              plotOutput("cardio_plot")
                              ),
                       
-              # creates the element for the main row, second half - this will also be a plot
+              # creates the element for the main row, second half - (this is a placeholder plot)
                       column(width = 6,
                              plotOutput("icu_plot")
                             )
@@ -45,32 +43,58 @@ ui <- fluidPage(
                                           value = 1, min = 1, max = 100),
                              ),
                       
-              # create the element for the right column
+              # create the element for the right column -(this is a placeholder plot)
                       column(width = 8,
                              plotOutput("dermatology_plot")
                              )
                       ),
              
-             # create the left box 
+             # create the left box with date selection
              fluidRow(column(width = 4, 
                              dateRangeInput("date_range_1", "Date Range",
                                             start = "2016-01-01",
                                             end = "2022-01-01")
                              ),
+                      
+              # create the bottom right box with text description
                       column(width = 8,
-                             textInput("text_input", "Text Goes Here", value = "text")
+                             textOutput("icu_text_placeholder")
                              )
              ),
     ),
 
 # A&E tab ----------------------------------------------------------------------
     # creates the navigation for A&E tab
-    tabPanel("A&E"
+    tabPanel("A&E",
              
+             # create the element for the left column
+             fluidRow(column(width = 4,
+                             numericInput("age_range_1", "Age Choice",
+                                          value = 1, min = 1, max = 100),
+             ),
+             
+             # create the element for the right column - this is a placeholder plot
+             column(width = 8,
+                    plotOutput("neurology_plot")
+                  )
+             ),
+             
+             # create the left box with date selection
+             fluidRow(column(width = 4, 
+                             dateRangeInput("date_range_1", "Date Range",
+                                            start = "2016-01-01",
+                                            end = "2022-01-01")
+                            ),
+                      
+              # create the bottom right box with text description
+                      column(width = 8,
+                            textOutput("ae_text_placeholder")
+                            )
+                      ),
              ),
 
 # Statistics tab ---------------------------------------------------------------
-    # creates the navigation for statistics tab
+    # creates the navigation for statistics tab - Ricardo
     tabPanel("Statistics"
              ),
     ),          
