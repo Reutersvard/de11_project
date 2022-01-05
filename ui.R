4703196b45ee4c5760194664561a4c3577fda7c4
Louise <louisegreen18@gmail.com>
2022-01-05 10:04:19 +0000

    A&E tab layout updated
    A&E tab layout updated - buttons moved to row alongside the top and graphs moved to row underneath

--- ui.R
+++ ui.R
@@ -105,6 +105,7 @@ body <- dashboardBody(
             fluidRow(column(width = 5,
                             br(),
                             plotOutput("beds_percentage_plot"),
+                            
                             # create the bottom right box with text description
                             textOutput("icu_text_placeholder"),
             ),
@@ -122,54 +123,50 @@ body <- dashboardBody(
 
             # creates a space between the top bar and the main page
             br(),
-
-            # create the element for the left column
-            sidebarLayout(
-
-              # creates the sidebar panel with the date, age and dept selection
-              sidebarPanel(
-                numericInput("age_range_1", "Age",
-                             value = 1, min = 1, max = 100),
-                
-                radioButtons("dept_type", "Department Type",
-                             choices = c("Accident and Emergency",
-                                        "Minor Injuries")),
-
-                sliderInput("date_range", label = "Date Range",
-                            min = as.Date("2016-01-01","%Y-%m-%d"),
-                            max = as.Date("2021-12-31","%Y-%m-%d"),
-                            value = c(as.Date("2016-01-01"),
-                                      as.Date("2021-12-31")),
-                            timeFormat="%Y-%m",
-                            step = 90
-                ),
-                actionButton("applyButton",
-                             "Apply Changes")
-              ),
-
-              # creates the main panel
-              mainPanel(
-
-                # create the element for the right column -(this is a placeholder plot)
-                fluidRow(column(width = 5,
-                                br(),
-                                plotOutput("ae_emergency_plot"),
-                                
-                                # create the bottom right box with text description
-                                textOutput("ae_text_placeholder")
-                              ),
-                         
-                         column(width = 5,
-                                br(),
-                                plotOutput("urology_plot"),
-                                
-                                # create the bottom right box with text description
-                                # textOutput("ae_text_placeholder")
-                                )
-                          )
-                      )
-              )
-
+            
+            # creates the element for the top row
+            fluidRow(column(width = 3,
+                            br(),
+                            numericInput("age_range_1", "Age",
+                                         value = 1, min = 1, max = 100)),
+                     column(width = 3,
+                            br(),
+                            radioButtons("dept_type", "Department Type",
+                                         choices = c("Accident and Emergency",
+                                                     "Minor Injuries"))),
+                     column(width = 3,
+                            br(),
+                            sliderInput("date_range", label = "Date Range",
+                                        min = as.Date("2016-01-01","%Y-%m-%d"),
+                                        max = as.Date("2021-12-31","%Y-%m-%d"),
+                                        value = c(as.Date("2016-01-01"),
+                                                  as.Date("2021-12-31")),
+                                        timeFormat="%Y-%m",
+                                        step = 90
+                                        )
+                            ),
+                     column(width = 3,
+                            br(),
+                            actionButton("applyButton",
+                                         "Apply Changes")
+                            )
+                      ),
+            
+            # creates the element for the main row, first half (this is a placeholder plot)
+            fluidRow(column(width = 6,
+                            br(),
+                            plotOutput("ae_emergency_plot"),
+                            
+                            # create the bottom right box with text description
+                            textOutput("ae_text_placeholder"),
+                            ),
+            
+            # creates the element for the main row, second half - (this is a placeholder plot)
+                      column(width = 6,
+                             br(),
+                             plotOutput("urology_plot")
+                            )
+                    ),
     ),
     
     # Statistics tab -----------------------------------------------------------
