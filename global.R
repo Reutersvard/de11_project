9ed5b5cc65b2e67cd88c0192eace392523cb955e
JonnyNelson1 <92695960+JonnyNelson1@users.noreply.github.com>
2022-01-05 15:56:04 +0000

    finale
    finale

--- global.R
+++ global.R
@@ -25,5 +25,7 @@ source("scripts/clean_data.R")
 clean_beds <- read_csv("clean_data/clean_beds.csv")
 clean_ae <- read_csv("clean_data/clean_ae.csv")
 
+# SelectInput Choices for global ------------------------------------------
+
 new_clean_ae <- clean_ae %>%
   select(attendance_greater8hrs, attendance_greater12hrs)
