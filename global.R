1da48aa89e81bbd0df24552fbe11b4e5c846a12f
malcheyne <92717988+malcheyne@users.noreply.github.com>
2022-01-06 15:02:17 +0000

    Update global.R
    commit out cleaning file

--- global.R
+++ global.R
@@ -14,7 +14,7 @@ library(lubridate)
 
 # Source scripts & read clean data ----------------------------------------
 
-source("scripts/clean_data.R")
+# source("scripts/clean_data.R")
 
 clean_beds <- read_csv("clean_data/clean_beds.csv", lazy = F)
 clean_ae <- read_csv("clean_data/clean_ae.csv", lazy = F)
